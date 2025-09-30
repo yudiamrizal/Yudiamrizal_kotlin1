e34b51127e027b51ebeef8fda2f6655f024b0441
yudi amrizal <yudiamrizalpnb@gmail.com>
2025-09-30 10:10:10 +0700

    Commit

--- src/Prak_Boolean.kt
+++ src/Prak_Boolean.kt
@@ -2,7 +2,4 @@ fun main(args: Array<String>) {
     var varBool : Boolean = true
     if(varBool)
         println("varBool bernilai " + varBool)
-    //var varInt : Int = 1
-   // if(varInt)
-//println("varInt bernilai " + varInt)
 }
--- src/Prak_Stirng.kt
+++ src/Prak_Stirng.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
+    /*
     val myName: String = "Yudi Amrizal"
     for(chr in myName){
         print(chr)
     }
     print('\n')
+    */
+    val s = "abc" + 1
+    println(s + "def")
 }
